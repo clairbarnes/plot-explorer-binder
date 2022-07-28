b89e4340dad78767e6e3e470a27202c3a831d87a
clairbarnes <clair.with.no.e@gmail.com>
2022-07-28 12:58:33 +0100

    Added environment kernel for local dev + fix_cmap method

--- plotting_methods.py
+++ plotting_methods.py
@@ -5,6 +5,16 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+import cartopy.crs as ccrs
+
+import warnings
+warnings.filterwarnings("ignore", message = ".+https://naciscdn.org/naturalearth/10m/physical/ne_10m_coastline.zip.+")     # ignore warning about downloading coastlines
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# define projection to be used in plotting
+crs_osgb = ccrs.TransverseMercator(approx = False, central_longitude = -2, central_latitude = 49, scale_factor = 0.9996012717,
+                                   false_easting = 400000, false_northing = -100000, globe = ccrs.Globe(datum = 'OSGB36', ellipse = 'airy'))
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # modified scatterplot to allow list of markers (taken from https://github.com/matplotlib/matplotlib/issues/11155)
 
@@ -85,3 +95,19 @@ rcm_colours.update({gcm : "w" for gcm in ['CNRM-CM5_r1i1p1', 'IPSL-CM5A-MR_r1i1p
 # add CMIP5-13 runs (grey symbols) to the RCM colour dictionary
 rcm_colours.update({gcm : "grey" for gcm in ['BCC-CSM1-1_r1i1p1', 'MRI-CGCM3_r1i1p1', 'CanESM2_r1i1p1', 'CESM1-BGC_r1i1p1', 'ACCESS1-3_r1i1p1', 
                                              'GFDL-ESM2G_r1i1p1', 'CCSM4_r1i1p1', 'CMCC-CM_r1i1p1', 'MPI-ESM-MR_r1i1p1'] if not gcm in rcm_colours.keys()})
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# replace diverging colourmap with sequential colourmap where values do not include 0
+    
+def fix_cmap(cmap, vmin, vmax):
+    
+    if not cmap in ["PuOr", "PuOr_r", "RdBu", "RdBu_r", "PRGn", "PRGn_r"]: return cmap
+    
+    if vmin < 0 and vmax > 0: # diverging colourmap
+        return cmap
+    
+    if vmin >= 0: # sequential, positive
+        return {"PuOr_r" : "Purples", "PuOr" : "Oranges", "RdBu_r" : "Reds", "RdBu" : "Blues", "PRGn" : "Greens", "PRGn_r" : "Purples"}[cmap]
+    
+    if vmax <= 0: # sequential, negative
+        return {"PuOr" : "Oranges_r", "PuOr_r" : "Purples_r", "RdBu" : "Reds_r", "RdBu_r" : "Blues_r", "PRGn" : "Purples", "PRGn_r" : "Greens"}[cmap]
